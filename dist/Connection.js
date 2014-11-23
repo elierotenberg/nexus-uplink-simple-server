f9bbfb762b330b873d5758c78d11aa376cfa7657
Elie Rotenberg <elie@rotenberg.io>
2014-11-23 16:54:31 +0100

    Fixed faulty gulpfile.js and upgraded deps.

--- dist/Connection.js
+++ dist/Connection.js
@@ -18,7 +18,7 @@ var _classProps = function (child, staticProps, instanceProps) {
   if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
 };
 
-var __NODE__ = !__BROWSER__;var __BROWSER__ = (typeof window === "object");var __PROD__ = !__DEV__;var __DEV__ = (process.env.NODE_ENV !== "production");var Promise = require("lodash-next").Promise;require("6to5/polyfill");module.exports = function (_ref) {
+require("6to5/polyfill");var Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;module.exports = function (_ref) {
   var UplinkSimpleServer = _ref.UplinkSimpleServer;
   var _ = require("lodash-next");
   var instanceOfSocketIO = require("./instanceOfSocketIO");
